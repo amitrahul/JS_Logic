3e2d2a87063e641e9f475d78467fc6390ec7efa8
Rahul Raj <raj.rahul686@gmail.com>
2023-06-02 08:07:17 +0000

    for loop method also added

--- Challenges/stringReverse.js
+++ Challenges/stringReverse.js
@@ -1,3 +1,16 @@
 //  Reversing a string can be easily achieved using split then combine reverse and join method
 
 const reverse = str => str.split("").reverse().join("");
+
+// using for loop : -
+
+const reverseString = (str) => {
+    let reversed = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+      const ch = str[i];
+      reversed += ch;
+    }
+    return reversed;
+  };
+  const revers = reverseString('javascript');
+  console.log(revers); // tpircsavaj
