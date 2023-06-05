405b8be94d1a2492284535d64e6164a8666ca6ea
Rahul Raj <raj.rahul686@gmail.com>
2023-06-05 11:37:28 +0000

    date methods added

---
+++ DataType/date.js
@@ -0,0 +1,21 @@
+//                                          date methods
+
+const date = new Date();  // Mon Jun 05 2023 17:01:29 GMT+0530 (India Standard Time)
+
+date.getDate();     // 5
+date.getMonth();    // 5
+date.getFullYear(); // 2023
+date.getHours();    // 17
+date.getMinutes(); // 1
+date.getSeconds();  // 29
+date.getMilliseconds(); // 671
+date.getTime();     // 1685964689671
+
+date.setDate(25);
+date.setMonth(8);
+date.setFullYear(2025);
+date.setHours(11);
+date.setMinutes(55);
+date.setSeconds(40);
+date.setMilliseconds(4500);
+date.setTime(236589745);
