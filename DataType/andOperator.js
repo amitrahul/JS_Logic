c87360019a48be79e7158aff4727c4f94cf4bb1b
Rahul Raj <raj.rahul686@gmail.com>
2023-06-12 18:07:39 +0000

    added and operator w.r.t if(true)

---
+++ DataType/andOperator.js
@@ -0,0 +1,10 @@
+/**              Replace if true statements with &&              */
+
+let flag = true;
+if(flag){
+    console.log(`i'm here`);
+}
+
+/** we can replace if through and operator */
+
+flag && console.log(`i'm here`);
