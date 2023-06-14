c9448e1960a247ac28d070773738be9ab48df59c
Rahul Raj <raj.rahul686@gmail.com>
2023-06-14 18:18:26 +0000

    added empty array concept

---
+++ Array/emptyArray.js
@@ -0,0 +1,12 @@
+/**         How to empty an array       */
+
+var arrayList =  ['a', 'b', 'c', 'd', 'e', 'f'];
+
+/** 1st way */
+arrayList = [];
+
+/** 2nd way */
+arrayList.length = 0;
+
+/** 3rd way  */
+arrayList.splice(0, arrayList.length);
