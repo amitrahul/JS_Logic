af477da5175642e6c9e9fc3719a0e4b0cba2c293
Rahul Raj <raj.rahul686@gmail.com>
2023-07-02 03:31:06 +0000

    added pop method

--- Array/emptyArray.js
+++ Array/emptyArray.js
@@ -1,6 +1,6 @@
 /**         How to empty an array       */
 
-var arrayList =  ['a', 'b', 'c', 'd', 'e', 'f'];
+var arrayList = ["a", "b", "c", "d", "e", "f"];
 
 /** 1st way */
 arrayList = [];
@@ -10,3 +10,8 @@ arrayList.length = 0;
 
 /** 3rd way  */
 arrayList.splice(0, arrayList.length);
+
+/** 4th way */
+while (arrayList.length > 0) {
+  arrayList.pop();
+}
